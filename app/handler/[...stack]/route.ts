fd77439b5a199687d23c1cfefdee572a98b0907a
24hwww\leonardo <24hwww@gmail.com>
2025-11-22 13:16:12 -0300

    actualiza1

--- app/handler/[...stack]/route.ts
+++ app/handler/[...stack]/route.ts
@@ -1,5 +1,5 @@
 import { stackServerApp } from "@/lib/stack";
 import { StackHandler } from "@stackframe/stack";
 
-export const GET = StackHandler({ app: stackServerApp });
-export const POST = StackHandler({ app: stackServerApp });
+export const GET = StackHandler({ app: stackServerApp, fullPage: true });
+export const POST = StackHandler({ app: stackServerApp, fullPage: true });
